8850c4adc6d0531046d2327d80405def6bd71e25
Alex Karacaoglu <alexkaracaoglu@alexs-mbp.home>
2019-03-02 10:59:53 -0500

    7.15 - Alamofire

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Alex Karacaoglu on 3/2/19.
+//  Copyright © 2019 Alex Karacaoglu. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey =  "b8af77d2f26e1935d0b62c00f5614154/"
